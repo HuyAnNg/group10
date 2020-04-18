bdca94da2396af3e122920fc0111a6ae1cda7f5a
Dennis Dinh Hung <dennisdinhhung@gmail.com>
2020-04-18 15:46:03 +0700

    Styling for home page

--- home.php
+++ home.php
@@ -2,25 +2,55 @@
 <html>
 <head>
 	<title></title>
+	
+	<meta charset="utf-8">
 </head>
+
 <link rel="stylesheet" href="home.css">
 <link rel="icon" href="image/shoes/back.png" type="image/gif" sizes="16x16">
-<body class="webpage" >
-	<div>
-		<div class="header" > 
-			<img src="image/logo.png" alt="loading" class="logo">
-			<a href=""><img src="image/cart_empty.png" alt="loading" class="cartLogo"></a>
-			<input type="text" class="search" placeholder="search">
-		</div>
 
-		<div class="body">
-			<nav class="content">
-				<p class="bcontent"> <span style="font-size: 30px,">POWERED BY THE PAST</span> <br>
-				Taking design from the 90’s, OZWEEGO <br> has defined midsole lines, a translucent <br> support tube, and adiPRENE technology.</p>
-				<a href="listpage1.php" class="ShopN">SHOP NOW</a>
-			</nav>
-		</div>
-	</div>
+<body class="webpage" >
+	
+	<header class="header">
+		
+		<a href="home.php"><img src="image/logo.svg" alt="loading" class="logo"></a>
+		
+		<nav class="nav_links">
+			<ul>
+				<li><a class="bar" href="#">ABOUT</a></li>
+				<li><a class="bar" href="#">SERVICES</a></li>
+				<li><a class="bar" href="#">CONTACT US</a></li>
+			</ul>
+		</nav>
+
+		<a href="#"><img src="image/cart_empty.svg" alt="loading" class="cart-logo"></a>
+				
+		<div class="search-container">
+    		<form action="#">
+      			<input type="text" placeholder="Search" name="search">
+      			<button type="submit"><i class="fa fa-search"></i></button>
+    		</form>
+  		</div>
+
+		<label for="nav-toggle" class="nav-toggle-label">
+            <span style="color: white"></span>
+        </label>
+	
+	</header>
+	
+	
+	<body>
+		<nav class="content">
+			<p class="bcontent"> <span style="font-size: 30px,">POWERED BY THE PAST</span>
+			Taking design from the 90’s,OZWEEGO has defined midsole lines, a translucent support tube, and adiPRENE technology.</p>
+			
+			<a href="listpage1.php"><button class="shop-now">SHOP NOW</button></a>
+		</nav>
+
+	</body>
+	
+	
+	
 	<footer>
 		<div class="copyright" align="center">
 			© 2019 Vellum America Inc.
