eebb86a9ee5857f6860f2b551930c9ab3e7c58e0
Dennis Dinh Hung <dennisdinhhung@gmail.com>
2020-04-20 13:42:22 +0700

    Clearing up the nav bar and add sections for better viewing exp

--- home.php
+++ home.php
@@ -8,33 +8,45 @@
 
 <link rel="stylesheet" href="home.css">
 <link rel="icon" href="image/shoes/back.png" type="image/gif" sizes="16x16">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
 
 <body class="webpage" >
 	
 	<header class="header">
 		
-		<a href="home.php"><img src="image/logo.svg" alt="loading" class="logo"></a>
+		<div class="row1">
+			<a href="home.php"><img src="image/logo.svg" alt="loading" class="logo"></a>
 		
-		<nav class="nav_links">
-			<ul>
-				<li><a class="bar" href="#">ABOUT</a></li>
-				<li><a class="bar" href="#">SERVICES</a></li>
-				<li><a class="bar" href="#">CONTACT US</a></li>
-			</ul>
-		</nav>
+			<nav class="nav_links">
+				<ul class="u">
+					<li><a class="bar" href="#">ABOUT</a></li>
+					<li><a class="bar" href="#">SERVICES</a></li>
+					<li><a class="bar" href="#">CONTACT US</a></li>
+				</ul>
+			</nav>
+		</div>
 
-		<a href="#"><img src="image/cart_empty.svg" alt="loading" class="cart-logo"></a>
+
+
+		<div class="row2">
+			<a href="#"><img src="image/cart_empty.svg" alt="loading" class="cart-logo"></a>
 				
-		<div class="search-container">
-    		<form action="#">
-      			<input type="text" placeholder="Search" name="search">
-      			<button type="submit"><i class="fa fa-search"></i></button>
-    		</form>
-  		</div>
+			<div class="search-container">
+				<form action="#">
+					<button class="search-button" type="submit"><i class="fa fa-search"></i></button>
+					<input class="input" type="text" placeholder="Search" name="search">
+				</form>
+			</div>
+				  
+			<a href="listpage1.php"><button class="shop-now">SHOP NOW</button></a>
+		
+		</div>
 
 		<label for="nav-toggle" class="nav-toggle-label">
             <span style="color: white"></span>
         </label>
+		
 	
 	</header>
 	
@@ -44,9 +56,8 @@
 			<p class="bcontent"> <span style="font-size: 30px,">POWERED BY THE PAST</span>
 			Taking design from the 90’s,OZWEEGO has defined midsole lines, a translucent support tube, and adiPRENE technology.</p>
 			
-			<a href="listpage1.php"><button class="shop-now">SHOP NOW</button></a>
+			
 		</nav>
-
 	</body>
 	
 	
