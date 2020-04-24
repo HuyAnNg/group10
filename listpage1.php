25a763127b8e9e048d18423a0cf72b5a68313c1d
Dennis Dinh Hung <dennisdinhhung@gmail.com>
2020-04-24 14:40:07 +0700

    Update listpage and its css

--- listpage1.php
+++ listpage1.php
@@ -9,23 +9,56 @@ require_once('./dbshoes.php');
  ?>
 <link rel="stylesheet" href="listpage1.css">
 <link rel="icon" href="image/Icon.png" type="image/gif" sizes="16x16">
-<body class="webpage">
-	<div>
-		<div class="header" > 
-			<a href="home.php"><img src="image/logo.png" alt="loading" class="logo"></a>
-			
-			<form action="" method="GET" class="search">
-			
-			<input type="text"  name="key" placeholder="search"style="width: 60%;; font-size: 30px;" ><input type="submit" name="search" value="search" style=" font-size: 30px;  "></form>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<header class="header">
+		
+		<div class="row1">
+			<a href="home.php"><img src="image/logo.svg" alt="loading" class="logo"></a>
+		
+			<nav class="nav_links">
+				<ul class="ul">
+					<li><a class="bar" href="#">ABOUT</a></li>
+					<li><a class="bar" href="#">SERVICES</a></li>
+					<li><a class="bar" href="#">CONTACT US</a></li>
+				</ul>
+			</nav>
 		</div>
 
-		<div class="left">
-			
-			<div class="block">
-			<div class="title" align="center">Brand</div>
-			
+
+
+		<div class="row2">
+			<a href="#"><img src="image/cart_empty.svg" alt="loading" class="cart-logo"></a>
 				
-					<div class="content">
+			<div class="search-container">
+				<form action="#">
+					<button class="search-button" type="submit"><i class="fa fa-search"></i></button>
+					<input class="input" type="text" placeholder="Search" name="search">
+				</form>
+			</div>
+				  
+			<a href="listpage1.php"><button class="shop-now">SHOP NOW</button></a>
+		
+		</div>
+
+		<label for="nav-toggle" class="nav-toggle-label">
+            <span style="color: white"></span>
+        </label>
+		
+	
+</header>
+
+
+<body class="webpage">
+	
+	<div class="offset-header"></div>
+	<div class="wall-header"><h1>CATEGORIES</h1></div>
+	<div class="offset-under"></div>
+
+	<div class="body">
+		<div class="wall">
+			<div class="wall-nav">
+					<div class="wall-list">
 						<ul><?php 
 						$sql = "select *from category";
 						$rows= query($sql);
@@ -35,21 +68,22 @@ require_once('./dbshoes.php');
 							<li><a href="listpage1.php?catid=<?=$rows[$i][0]?>"><?=$rows[$i][1]?></a></li>
 							<?php } ?>
 						</ul>
-					</div>
-					
-				
+					</div>	
 			</div>
+			
 			<br>
-			<div >
-						<a href="login.php" style="color: black; margin-left: 50px ">Log In as Admin?</a>
+			
+			<div class="login">
+				<a href="login.php">Log In as Admin?</a>
 			</div>
 			
 		</div>
-		<div class="right" >
-				<p class="font" style="font-size: 30px">ALL PRODUCTS</p>
+		
+		<div class="prod-area">
 				
 				<div>
-						<table border = "1" >
+						<table class="prod-table">
+							
 							<?php 
 								if (isset($_GET['catid'])) {
 									# code...
@@ -67,13 +101,16 @@ require_once('./dbshoes.php');
 								$name = query($sql);
 								for ($i=0; $i <count($name) ; $i++) { 
 							 ?>
+							
 							<tr style="width: 1000px">
-								<th>
-									<a href="productdetail.php?productid=<?=$name[$i][0]?>" >
-										<img src="<?=$name[$i][3]?>" alt="loading" width="300" height="300"class="zoom">
-										<div align="center" class="shoesname"><?=$name[$i][2]?></div>
-									</a>
-								</th>
+								
+							<th>
+								<a class="shoesname" href="productdetail.php?productid=<?=$name[$i][0]?>" >
+									<img src="<?=$name[$i][3]?>" alt="loading" width="300" height="300"class="prod-pic">
+									<div class="shoesname"><?=$name[$i][2]?></div>
+								</a>
+							</th>
+								
 								<th >
 									<div style="margin-left: 300px;" >Price: $<?=$name[$i][5]?></div>
 
